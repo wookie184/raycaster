72c7f46271d934235166cab423bb570f9381f4ad
wookie184 <wookie1840@gmail.com>
2022-02-05 20:01:37 +0000

    Improve formatting in matrix tests and add formatting specifiers ready for blackening

--- tests/test_matrix.py
+++ tests/test_matrix.py
@@ -1,15 +1,17 @@
 from ..src.raycaster.vector import Tuple
 from ..src.raycaster.matrix import Matrix
-import sys
-
-EPSILON = sys.float_info.min
 
 class TestMatrix:
     def test_create(self):
         m = Matrix([0]*16)
 
     def test_create_2x2(self):
-        m = Matrix([-3, 5, 1, -2])
+        # fmt: off
+        m = Matrix([
+            -3,  5,
+             1, -2,
+        ])
+        # fmt: on
         assert m[0, 0] == -3
         assert m[0, 1] == 5
         assert m[1, 0] == 1
@@ -37,8 +39,8 @@ class TestMatrix:
         assert a == b
 
     def test_equality_float(self):
-        a = Matrix([0, 0, EPSILON, 0])
-        b = Matrix([0, 0, 0, 0])
+        a = Matrix([0, 100, 1e-50, 0])
+        b = Matrix([0, 100 + 1e-10, 0, 0])
         assert a == b
 
     def test_get_col(self):
@@ -100,17 +102,48 @@ class TestMatrix:
         assert m[-1, :] == [12, 13, 14, 15]
 
     def test_multiply_self(self):
-        a = Matrix([1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2])
-        b = Matrix([-2, 1, 2, 3, 3, 2, 1, -1, 4, 3, 6, 5, 1, 2, 7, 8])
-        assert a * b == Matrix( [20, 22, 50, 48, 44, 54, 114, 108, 40, 58, 110, 102, 16, 26, 46, 42])
+        # fmt: off
+        a = Matrix([
+            1, 2, 3, 4,
+            5, 6, 7, 8, 
+            9, 8, 7, 6, 
+            5, 4, 3, 2,
+        ])
+        b = Matrix([
+            -2,  1,  2,  3,
+             3,  2,  1, -1,
+             4,  3,  6,  5,
+             1,  2,  7,  8,
+        ])
+        assert a * b == Matrix([
+            20,  22,  50,  48,
+            44,  54, 114, 108,
+            40,  58, 110, 102,
+            16,  26,  46,  42,
+        ])
+        # fmt: on
 
     def test_multiply_tuple(self):
-        a = Matrix([1, 2, 3, 4, 2, 4, 4, 2, 8, 6, 4, 1, 0, 0, 0, 1])
+        # fmt: off
+        a = Matrix([
+            1, 2, 3, 4,
+            2, 4, 4, 2,
+            8, 6, 4, 1,
+            0, 0, 0, 1,
+        ])
+        # fmt: on
         b = Tuple(1, 2, 3, 1)
         assert a * b == Tuple(18, 24, 33, 1)
 
     def test_multiply_identity(self):
-        a = Matrix([0, 1, 2, 4, 1, 2, 4, 8, 2, 4, 8, 16, 4, 8, 16, 32])
+        # fmt: off
+        a = Matrix([
+            0,  1,  2,  4,
+            1,  2,  4,  8,
+            2,  4,  8, 16,
+            4,  8, 16, 32,
+        ])
+        # fmt: on
         b = Matrix.identity()
         assert a * b == a
         
@@ -118,8 +151,20 @@ class TestMatrix:
         assert b * c == c
 
     def test_transpose(self):
-        a = Matrix([0, 9, 3, 0, 9, 8, 0, 8, 1, 8, 5, 3, 0, 0, 5, 8])
-        b = Matrix([0, 9, 1, 0, 9, 8, 8, 0, 3, 0, 5, 5, 0, 8, 3, 8])
+        # fmt: off
+        a = Matrix([
+            0, 9, 3, 0, 
+            9, 8, 0, 8, 
+            1, 8, 5, 3, 
+            0, 0, 5, 8,
+        ])
+        b = Matrix([
+            0, 9, 1, 0, 
+            9, 8, 8, 0, 
+            3, 0, 5, 5, 
+            0, 8, 3, 8,
+        ])
+        # fmt: on
 
         assert a.transpose() == b
 
@@ -127,26 +172,61 @@ class TestMatrix:
         assert Matrix.identity().transpose() == Matrix.identity()
 
     def test_determinant_2x2(self):
-        a = Matrix([1, 5, -3, 2])
+        # fmt: off
+        a = Matrix([
+             1, 5,
+            -3, 2,
+        ])
+        # fmt: off
         assert a.determinant() == 17
 
     def test_submatrix(self):
-        a = Matrix([1, 5, 0, -3, 2, 7, 0, 6, -3])
-        assert a.submatrix(0, 2) == Matrix([-3, 2, 0, 6])
+        # fmt: off
+        a = Matrix([
+             1,  5,  0,
+            -3,  2,  7,
+             0,  6, -3,
+        ])
+        assert a.submatrix(0, 2) == Matrix([
+            -3, 2,
+             0, 6,
+        ])
 
-        b = Matrix([-6, 1, 1, 6, -8, 5, 8, 6, -1, 0, 8, 2, -7, 1, -1, 1])
-        c = Matrix([-6, 1, 6, -8, 8, 6, -7, -1, 1])
-        assert b.submatrix(2, 1) == c
+        b = Matrix([
+            -6,  1,  1,  6,
+            -8,  5,  8,  6,
+            -1,  0,  8,  2,
+            -7,  1, -1,  1,
+        ])
+
+        assert b.submatrix(2, 1) == Matrix([
+            -6,  1,  6,
+            -8,  8,  6,
+            -7, -1,  1,
+        ])
+        # fmt: on
 
     def test_minor(self):
-        a = Matrix([3, 5, 0, 2, -1, -7, 6, -1, 5])
+        # fmt: off
+        a = Matrix([
+             3,  5,  0,
+             2, -1, -7,
+             6, -1,  5,
+        ])
+        # fmt: on
         b = a.submatrix(1, 0)
 
         assert b.determinant() == 25
         assert a.minor(1, 0) == 25
 
     def test_cofactor(self):
-        a = Matrix([3, 5, 0, 2, -1, -7, 6, -1, 5])
+        # fmt: off
+        a = Matrix([
+             3,  5,  0, 
+             2, -1, -7, 
+             6, -1,  5,
+        ])
+        # fmt: on
 
         assert a.minor(0, 0) == -12
         assert a.cofactor(0, 0) == -12
@@ -155,14 +235,28 @@ class TestMatrix:
         assert a.cofactor(1, 0) == -25
 
     def test_determinant_3x3(self):
-        a = Matrix([1, 2, 6, -5, 8, -4, 2, 6, 4])
+        # fmt: off
+        a = Matrix([
+             1,  2,  6, 
+            -5,  8, -4, 
+             2,  6,  4,
+        ])
+        # fmt: on
+
         assert a.cofactor(0, 0) == 56
         assert a.cofactor(0, 1) == 12
         assert a.cofactor(0, 2) == -46
         assert a.determinant() == -196
 
     def test_determinant_4x4(self):
-        a = Matrix([-2, -8, 3, 5, -3, 1, 7, 3, 1, 2, -9, 6, -6, 7, 7, -9])
+        # fmt: off
+        a = Matrix([
+            -2, -8,  3,  5,
+            -3,  1,  7,  3,
+             1,  2, -9,  6,
+            -6,  7,  7, -9,
+        ])
+        # fmt: on
         assert a.cofactor(0, 0) == 690
         assert a.cofactor(0, 1) == 447
         assert a.cofactor(0, 2) == 210
@@ -170,16 +264,37 @@ class TestMatrix:
         assert a.determinant() == -4071
 
     def test_invertability(self):
-        a = Matrix([6, 4, 4, 4, 5, 5, 7, 6, 4, -9, 3, -7, 9, 1, 7, -6])
+        # fmt: off
+        a = Matrix([
+             6,  4,  4,  4, 
+             5,  5,  7,  6,
+             4, -9,  3, -7, 
+             9,  1,  7, -6,
+        ])
+        # fmt: on
         assert a.determinant() == -2120
         assert a.is_invertible()
-
-        b = Matrix([-4, 2, -2, -3, 9, 6, 2, 6, 0, -5, 1, -5, 0, 0, 0, 0])
+        
+        # fmt: off
+        b = Matrix([
+            -4,  2, -2, -3,
+             9,  6,  2,  6,
+             0, -5,  1, -5, 
+             0,  0,  0,  0,
+        ])
+        # fmt: on
         assert b.determinant() == 0
         assert not b.is_invertible()
 
     def test_inverse(self):
-        a = Matrix([-5, 2, 6, -8, 1, -5, 1, 8, 7, 7, -6, -7, 1, -3, 7, 4])
+        # fmt: off
+        a = Matrix([
+            -5,  2,  6, -8, 
+             1, -5,  1,  8, 
+             7,  7, -6, -7, 
+             1, -3,  7,  4,
+        ])
+        # fmt: on
         b = a.inverse()
 
         assert a.determinant() == 532
@@ -189,27 +304,65 @@ class TestMatrix:
         assert a.cofactor(3, 2) == 105
         assert b[2, 3] == 105/532
 
+        # fmt: off
         exp = Matrix([
              0.21805,  0.45113,  0.24060, -0.04511,
-            -0.80827, -1.45677, -0.44361, 0.52068, 
-            -0.07895, -0.22368, -0.05263, 0.19737,
-            -0.52256, -0.81391, -0.30075, 0.30639,
+            -0.80827, -1.45677, -0.44361,  0.52068, 
+            -0.07895, -0.22368, -0.05263,  0.19737,
+            -0.52256, -0.81391, -0.30075,  0.30639,
         ])
+        # fmt: on
         assert b.is_close(exp)
         assert not a.is_close(exp)
 
     def test_more_inverse(self):
-        a = Matrix([8, -5, 9, 2, 7, 5, 6, 1, -6, 0, 9, 6, -3, 0, -9, -4])
-        exp = Matrix([-0.15385, -0.15385, -0.28205, -0.53846, -0.07692, 0.12308, 0.02564, 0.03077, 0.35897, 0.35897, 0.4359, 0.92308, -0.69231, -0.69231, -0.76923, -1.92308])
+        # fmt: off
+        a = Matrix([
+             8, -5,  9,  2,
+             7,  5,  6,  1, 
+            -6,  0,  9,  6, 
+            -3,  0, -9, -4,
+        ])
+        exp = Matrix([
+            -0.15385, -0.15385, -0.28205, -0.53846,
+            -0.07692,  0.12308,  0.02564,  0.03077,
+             0.35897,  0.35897,  0.43590,  0.92308,
+            -0.69231, -0.69231, -0.76923, -1.92308,
+        ])
+        # fmt: on
         assert a.inverse().is_close(exp)
 
-        b = Matrix([9, 3, 0, 9, -5, -2, -6, -3, -4, 9, 6, 4, -7, 6, 6, 2])
-        exp = Matrix([-0.04074, -0.07778, 0.14444, -0.22222, -0.07778, 0.03333, 0.36667, -0.33333, -0.02901, -0.1463, -0.10926, 0.12963, 0.17778, 0.06667, -0.26667, 0.33333])
+        # fmt: off
+        b = Matrix([
+             9,  3,  0,  9, 
+            -5, -2, -6, -3, 
+            -4,  9,  6,  4, 
+            -7,  6,  6,  2,
+        ])
+        exp = Matrix([
+            -0.04074, -0.07778,  0.14444, -0.22222,
+            -0.07778,  0.03333,  0.36667, -0.33333,
+            -0.02901, -0.14630, -0.10926,  0.12963,
+             0.17778,  0.06667, -0.26667,  0.33333,
+        ])
+        # fmt: on
         assert b.inverse().is_close(exp)
 
     def test_mul_inverse(self):
-        a = Matrix([3, -9, 7, 3, 3, -8, 2, -9, -4, 4, 4, 1, -6, 5, -1, 1])
-        b = Matrix([8, 2, 2, 2, 3, -1, 7, 0, 7, 0, 5, 4, 6, -2, 0, 5]) 
+        # fmt: off
+        a = Matrix([
+             3, -9,  7,  3, 
+             3, -8,  2, -9, 
+            -4,  4,  4,  1, 
+            -6,  5, -1,  1,
+        ])
+        b = Matrix([
+             8,  2,  2,  2,
+             3, -1,  7,  0,
+             7,  0,  5,  4, 
+             6, -2,  0,  5,
+        ]) 
+        # fmt: on
         c = a * b
         assert c * b.inverse() == a
 
