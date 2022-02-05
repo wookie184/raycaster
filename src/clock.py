6b6670bca8f296ae6533832519b02b8efc2be6f5
wookie184 <wookie1840@gmail.com>
2022-02-05 19:33:00 +0000

    Add clock example

---
+++ src/clock.py
@@ -0,0 +1,20 @@
+from raycaster.transformation import rotation_z
+from raycaster.vector import Colour, point, vector
+from raycaster.canvas import Canvas
+import math
+
+
+def main():
+    c = Canvas(500, 500)
+    top = point(0, 1, 0) * 200
+    for rot in range(12):
+        theta = rot * (math.pi/6)
+        rotation = rotation_z(theta)
+
+        res = rotation * top + vector(250, 250, 0)
+        c.write(int(res.x), int(res.y), Colour(255, 0, 0))
+    c.save_ppm("clock_render.ppm")
+
+
+if __name__ == "__main__":
+    main()
