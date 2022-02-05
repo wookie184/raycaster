103b3dc92f2eceb529323e5e903662e37f0c4e6f
wookie184 <wookie1840@gmail.com>
2022-02-05 19:31:05 +0000

    Complete projectile test

--- src/projectile.py
+++ src/projectile.py
@@ -1,4 +1,6 @@
-from vector import vector, point, Tuple
+from raycaster.vector import Colour
+from raycaster.vector import vector, point, Tuple
+from raycaster.canvas import Canvas
 from typing import NamedTuple
 
 class Projectile(NamedTuple):
@@ -13,18 +15,25 @@ class Environment(NamedTuple):
 def main():
     proj = Projectile(
         position=point(0, 1, 0),
-        velocity=vector(1, 1, 0).normalize()
+        velocity=vector(1, 0.5, 0).normalize()
     )
     env = Environment(
-        vector(0, -0.1, 0),
-        vector(-0.01, 0, 0)
+        vector(0, -0.0001, 0),
+        vector(-0.00005, 0, 0)
     )
 
-    while proj.position.y > 0:
+    canvas = Canvas(10000, 1500)
+
+    while proj.position.y >= 0:
         position = proj.position + proj.velocity
         velocity = proj.velocity + env.gravity + env.wind
         proj = Projectile(position, velocity)
-        print(proj.position)
+        try:
+            canvas.write(int(proj.position.x), canvas.height-int(proj.position.y), Colour(1, 0, 0))
+        except IndexError:
+            pass
+
+    canvas.save_ppm("./projectile_plot.ppm")
 
 
 if __name__ == "__main__":
