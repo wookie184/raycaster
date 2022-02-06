9cfd611b2cb201a9af96d5d7ce2d92ecc5cc6cc6
wookie184 <wookie1840@gmail.com>
2022-02-06 12:53:17 +0000

    Save ppm files to render directory

--- src/clock.py
+++ src/clock.py
@@ -1,8 +1,12 @@
 import math
+from pathlib import Path
 
 from raycaster.canvas import Canvas
 from raycaster.transformation import rotation_z
 from raycaster.vector import Colour, point, vector
+
+FILE_DIR = Path(__file__).parent.parent / "renders"
+FILE_DIR.mkdir(exist_ok=True)
 
 
 def main():
@@ -14,7 +18,7 @@ def main():
 
         res = rotation * top + vector(250, 250, 0)
         c.write(int(res.x), int(res.y), Colour(255, 0, 0))
-    c.save_ppm("clock_render.ppm")
+    c.save_ppm(FILE_DIR / "clock_render.ppm")
 
 
 if __name__ == "__main__":
--- src/projectile.py
+++ src/projectile.py
@@ -1,8 +1,12 @@
 import contextlib
+from pathlib import Path
 from typing import NamedTuple
 
 from raycaster.canvas import Canvas
 from raycaster.vector import Colour, Tuple, point, vector
+
+FILE_DIR = Path(__file__).parent.parent / "renders"
+FILE_DIR.mkdir(exist_ok=True)
 
 
 class Projectile(NamedTuple):
@@ -16,10 +20,12 @@ class Environment(NamedTuple):
 
 
 def main():
-    proj = Projectile(position=point(0, 1, 0), velocity=vector(1, 0.5, 0).normalize())
-    env = Environment(vector(0, -0.0001, 0), vector(-0.00005, 0, 0))
+    proj = Projectile(
+        position=point(0, 1, 0), velocity=vector(1, 1, 0).normalize() * 1.3
+    )
+    env = Environment(vector(0, -0.001, 0), vector(-0.0005, 0, 0))
 
-    canvas = Canvas(10000, 1500)
+    canvas = Canvas(1000, 500)
 
     while proj.position.y >= 0:
         position = proj.position + proj.velocity
@@ -32,7 +38,7 @@ def main():
                 Colour(1, 0, 0),
             )
 
-    canvas.save_ppm("./projectile_plot.ppm")
+    canvas.save_ppm(FILE_DIR / "projectile_plot.ppm")
 
 
 if __name__ == "__main__":
